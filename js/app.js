173bc58e08bbdfc2d92cd4aa2efc110098a3b92f
mspil88 <matthewspillane88@gmail.com>
2022-10-07 15:39:50 +0100

    refactoring and comments added

--- js/app.js
+++ js/app.js
@@ -13,58 +13,21 @@
  * 
 */
 
-/**
- * Comments should be present at the beginning of each procedure and class.
- * Great to have comments before crucial code sections within the procedure.
+/* GLOBAL VARIABLES:
+    sections: all sections in the document
+    navBar: the navbar in the document
+    OBSERVER_THRESHOLD: the threshold (%) for the intersection observer
+
 */
-
-/**
- * Define Global Variables
- * 
-*/
-
-
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
-
-
-
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
-
-// build the nav
-
-
-// Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
-
-
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
-
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
 
 const sections = Array.from(document.getElementsByTagName("section"));
 const navBar = document.querySelector("#navbar__list");
+const OBSERVER_THRESHOLD = 0.5;
 
 
-//creating an unordered list or nav from the sections array nad think about how I can test whether a section is in the viewport
-
+/*addSmoothScrolling adds smooth scrolling to the nav bar links by iterating through all links and adding an event listener which disables 
+  default behaviour of the anchor tag and implements scrollIntoView on the link
+*/
 const addSmoothScrolling = () => {
     const navLinks = document.querySelectorAll(".nav__link");
     navLinks.forEach(link=> {
@@ -76,6 +39,12 @@ const addSmoothScrolling = () => {
     })
 }
 
+/*
+ Dynamically creates the nav bar links based on the sections in the document, implements links as list items, sets the text content based on the 
+ data-nav attribution on the section, sets the class name, href and sets a new attribute 'link-to' which is used by the addSmoothScrolling function.
+ Uses a listElement array where the list elements are appended, this structure is then appended in one go to the nav bar using the spread operator.
+ smooth scrolling function invoked after to add the event listener to the new element
+*/
 const createNav = () => {
 
     let listElements = [];
@@ -87,12 +56,6 @@ const createNav = () => {
         a.setAttribute("link-to", section.getAttribute("id"))
         a.className = "nav__link menu__link";
         a.href = `#${section.getAttribute("id")}`
-
-        // li.addEventListener("click", (event)=> {
-        //     event.preventDefault();
-        //     a.scrollIntoView(true);
-        // })
-        
         li.append(a);
         listElements.push(li)
         }
@@ -102,14 +65,16 @@ const createNav = () => {
     addSmoothScrolling();
 }
 
-createNav();
+/* 
+ Instance of the intersection observer with a callback function checking if an element is intersecting (in view) with a threshold equal to the 
+ value of the OBSERVE_THRESHOLD variable (currenty 0.5). If the element is interesection then we toggle the classList to 'your active class' else
+ we remove that value. This initiates the CSS effects when the section is sufficiently in view
+*/
 
 const observer = new IntersectionObserver(
     entries => {
-        entries.forEach(entry => {
-            
+        entries.forEach(entry => {  
             if(entry.isIntersecting) {
-                console.log(entry.target)
                 entry.target.classList.toggle("your-active-class", entry.isIntersecting);
             }
             else {
@@ -118,15 +83,14 @@ const observer = new IntersectionObserver(
     })
     },
     {
-        threshold: 0.5,
+        threshold: OBSERVER_THRESHOLD,
     }
 )
 
+//create the nav bar
+createNav();
+
+//iterate over the sections in the document and initiate the observe for each
 sections.forEach(section => {
     observer.observe(section)
 })
-
-
-console.log("nav")
-Array.from(navBar).forEach(itm => console.log(itm))
-
